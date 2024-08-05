b867e898d632fc118def90769372dd1c3feed76a
codewithdolly <intensedolly321@gmail.com>
2024-08-05 15:20:01 +0530

    not found page implimentation

---
+++ src/app/not-found.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function notFound() {
+  return (
+   <>
+    <div>Page Not found</div>
+    <a href="/">Go to home page</a>
+    </>
+
+  )
+}
+
+export default notFound
